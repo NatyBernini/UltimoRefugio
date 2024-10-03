573e48ce04f667c6448c50a3e373b60c6ab090d2
NatyBernini <110041227+NatyBernini@users.noreply.github.com>
2024-10-03 12:59:18 -0300

    Add files via upload

--- UltimoRefugio/Player/Scripts/state_attack.gd
+++ UltimoRefugio/Player/Scripts/state_attack.gd
@@ -9,6 +9,7 @@ var attacking : bool = false
 @onready var idle : State = $"../idle"
 @onready var animation_player : AnimationPlayer = $"../../AnimationPlayer"
 @onready var walk : State = $"../walk"
+@onready var hurt_box : HurtBox = $"../../Interactions/hurt_box"
 func Enter() -> void:
 	player.UpdateAnimation("attack")
 	attack_anim.play( "attack_" + player.AnimDirection() )
@@ -17,11 +18,15 @@ func Enter() -> void:
 	audio.pitch_scale = randf_range(0.9, 1.1)
 	audio.play()
 	attacking = true
+	
+	await get_tree().create_timer( 0.075 ).timeout 
+	hurt_box.monitoring = true
 	pass
 	
 func Exit() -> void:
 	animation_player.animation_finished.disconnect( EndAttack )
 	attacking = false
+	hurt_box.monitoring = false
 	pass
 	
 func Process( _delta : float ) -> State:
